3e5343ab3ec7dba31053c0260a9c729c839eb708
David Vychanek <vychanek@vychanek.cz>
2023-11-22 09:40:58 +0100

    prvni komit

---
+++ script.js
@@ -0,0 +1,32 @@
+(function($){
+     $(function() {
+// ==== sem se píše text
+
+// === mobile navigation ====
+
+// $(".jq--nav-icon").click(function(){
+    
+//      // $(".nav-background").slideToggle(); 
+//      $(".mobile-nav-back").fadeToggle(); 
+//      $("nav ul").fadeToggle(); 
+       
+//     });   
+    
+   /*podmínky změna hamburgeru na cross a zpět */
+        
+//   $(".jq--image-hamburger").click(function(){
+      
+      
+//       if($(".jq--image-hamburger").attr("src") == "images/hamburgerMenu.png" )
+//      {     
+//     $( $(".jq--image-hamburger").attr("src","images/crossMenu.png"));
+//   }
+//      else
+//      {
+//    $( $(".jq--image-hamburger").attr("src","images/hamburgerMenu.png"));
+//      }
+
+//      });
+
+});
+    })(jQuery);
