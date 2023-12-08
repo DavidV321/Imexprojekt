a932e079006a5b88e7de616dcef6d3e972e0c8d5
David Vychanek <vychanek@vychanek.cz>
2023-12-08 23:22:21 +0100

    comitt 3

--- menu.php
+++ menu.php
@@ -4,7 +4,7 @@
     if ($instance_stranky->get_menu() != "") {
         $escaped_menu = htmlspecialchars($instance_stranky->get_menu());
         $escaped_id = htmlspecialchars($instance_stranky->get_id());
-        echo "<li><a href='?id-stranky={$escaped_id}'>{$escaped_menu}</a></li>";
+        echo "<li><a href='{$escaped_id}'>{$escaped_menu}</a></li>";
     }
     // echo "<li><a href='?id-stranky={$stranka->get_id()}'>{$stranka->get_menu()}</a></li>";
 }
